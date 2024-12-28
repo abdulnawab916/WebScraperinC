9c961cb9c90802035f87d38ca4b266b9b264591f
Abdul Hanan Nawab <abdulnawab98@gmail.com>
2024-12-28 15:14:15 -0800

    A: Added callback functions

--- scraper.c
+++ scraper.c
@@ -1,51 +1,60 @@
 /*
-	A project written in C by Abdul Nawab
+	A web-scraper written in C by Abdul Nawab
 */
 
 #include <stdio.h>
 #include <curl/curl.h>
+#include <stdlib.h>
 
+// Callback function header, we have to follow the prototype below
+// of the parameters
+
+// I initialize the number of Chunks to be 1, and this will be incremented each 
+// time that the callback function is invoked
+// I could not define this locally because it would just reset the variable to be '1'
+// on each invokation from the function
+int counterForChunks = 1;
+
+size_t getData(char *buffer, size_t itemSize, size_t nItems, void* ignoreThis);
+
+// Entry point
 int main(void) {
-    CURL *curl; // setting up that first curl instance var
-    CURLcode res; // the response variable
+	CURL *curl = curl_easy_init();
+	if (!curl){
+		// Print to error stream
+		fprintf(stderr, "Initialization failed\n");
+}
+	// Generates initial response from trying to acquire data from the following URL
+	curl_easy_setopt(curl, CURLOPT_URL, "https://abdulnawab916.github.io/PersonalWebsite/");
+	
+	// The callback is invoked however many times that it is needed
+	// There can be a maximum limit on it, but that is not needed in our case
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, getData);
+	CURLcode responseCode = curl_easy_perform(curl);
 
-    /* Initialize libcurl */
-    curl_global_init(CURL_GLOBAL_DEFAULT); 
-	// we always have to globally initialize the variable
-
-    /* Get a CURL handle */
-    curl = curl_easy_init();
-    if(curl) {
-        /* Set URL to fetch */
-        curl_easy_setopt(curl, CURLOPT_URL, "https://www.example.com");
-
-        /*
-         * Forcing the HTTPS certificate check to be strict is usually recommended,
-         * but if you are working with a self-signed cert or local server, you can
-         * disable verification with something like:
-         *
-         * curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-         * curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-         */
-
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-
-        /* Check for errors */
-        if(res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-        }
-
-        /* Clean up */
-        curl_easy_cleanup(curl);
-    }
-
-	// We always have to call curl_global_cleanup() at the end of 
-	// every main function
-    /* Cleanup global libcurl state */
-    curl_global_cleanup();
-
-    return 0;
+	// Basic error handling
+	if (responseCode != CURLE_OK){
+		fprintf(stderr, "Damn bruh, it ain't work!: %s\n", curl_easy_strerror(responseCode));
+	}
+	curl_easy_cleanup(curl);
 }
 
+size_t getData(char *buffer, size_t itemSize, size_t nItems, void* ignoreThis){
+	
+	// Calculation to compute the number of bytes that we are working with
+	size_t totalBytes = itemSize * nItems;
+	printf("Chunk Number: %d\n Size: (%zu) bytes\n", counterForChunks, totalBytes);
+	counterForChunks++;
+	return totalBytes;
+	// Iterate through the buffer and print out the contents of the current chunk 
+
+}
+
+/*
+Resources:
+'Easy Networking in C (libcurl)':
+https://www.youtube.com/watch?v=daA-KBKfJ_o
+'curl' Documentation:
+https://curl.se/libcurl/c/CURLOPT_WRITEDATA.html#:~:text=
+If%20you%20use%20the%20CURLOPT_WRITEFUNCTION,(3)%20when%20writing%20data.
+*/
